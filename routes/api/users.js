889da97b1980a13f641fd418102a833507b92878
jusselman <usselmanjosh@gmail.com>
2019-10-31 20:58:12 -0700

    user registration api

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,71 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 // GET api users, Test route, public access //
-router.get('/', (req, res) => res.send('User route'));
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include valid email').isEmail(),
+    check('password', 'Please enter password with 6 or more characters').isLength({ min: 6 })
+],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { name, email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+
+            if (user) {
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'User already exists' }] });
+            }
+
+            const avatar = gravatar.url(email, {
+                // size, rating, default //
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            })
+
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            });
+
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            // Return jsonwebtoken //
+            res.send('User Registered');
+
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+
+
+
+
+
+
+
+
+    }
+);
 
 
 module.exports = router;
